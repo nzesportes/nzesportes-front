3cf22646ecfb94e6caa98d422acda32f69add782
rafaelpeinado <rafael.peinado.silva@gmail.com>
2021-04-06 09:27:39 -0300

    fix: retirando import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardModule } from './dashboard/dashboard.module';
-import {DashboardComponent} from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
