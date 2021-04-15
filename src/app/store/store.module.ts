9df7748e85d82911922cf6a43004492b7f44ac95
rafaelpeinado <rafael.peinado.silva@gmail.com>
2021-04-15 03:50:38 -0300

    feat: footer e social media criados

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -1,20 +1,32 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { StoreRoutingModule } from './store-routing.module';
-import { StoreComponent } from './store.component';
-import { NavBarComponent } from './template/nav-bar/nav-bar.component';
-import { HeaderComponent } from './template/header/header.component';
-import { BannerComponent } from './template/banner/banner.component';
-import { HomeContentComponent } from './template/home-content/home-content.component';
-import { ContactUsComponent } from './template/contact-us/contact-us.component';
+import {StoreRoutingModule} from './store-routing.module';
+import {StoreComponent} from './store.component';
+import {NavBarComponent} from './template/nav-bar/nav-bar.component';
+import {HeaderComponent} from './template/header/header.component';
+import {BannerComponent} from './template/banner/banner.component';
+import {HomeContentComponent} from './template/home-content/home-content.component';
+import {ContactUsComponent} from './template/contact-us/contact-us.component';
+import {SocialMediaComponent} from './template/social-media/social-media.component';
+import { FooterComponent } from './template/footer/footer.component';
 
 
 @NgModule({
-  declarations: [StoreComponent, NavBarComponent, HeaderComponent, BannerComponent, HomeContentComponent, ContactUsComponent],
+  declarations: [
+    StoreComponent,
+    NavBarComponent,
+    HeaderComponent,
+    BannerComponent,
+    HomeContentComponent,
+    ContactUsComponent,
+    SocialMediaComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule,
     StoreRoutingModule
   ]
 })
-export class StoreModule { }
+export class StoreModule {
+}
