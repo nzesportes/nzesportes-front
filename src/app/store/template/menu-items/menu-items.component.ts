15e78af9e726ade2d9a479b36797ea4460b20f52
rafaelpeinado <rafael.peinado.silva@gmail.com>
2021-04-21 22:36:10 -0300

    feat: itens de menu e sidebar finalizados. fix: close em cart-preview

--- src/app/store/template/menu-items/menu-items.component.ts
+++ src/app/store/template/menu-items/menu-items.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-menu-items',
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-items.component.scss']
 })
 export class MenuItemsComponent implements OnInit {
-
-  constructor() { }
+  
+  constructor() {
+  }
 
   ngOnInit(): void {
+
+  }
+
+  changeStateItemMenu(itemMenu: HTMLElement): void {
+    if (itemMenu.className === 'mega-menu') {
+      itemMenu.className = 'mega-menu menu-opened';
+    } else if (itemMenu.className === 'mega-menu menu-opened') {
+      itemMenu.className = 'mega-menu';
+    }
   }
 
 }
