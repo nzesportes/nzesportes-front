22e725a148ba0fa67fa2abf02c6bc365cd639826
nfandre <andrelive.05@hotmail.com>
2021-05-11 23:58:22 -0300

    feat: reactive forms categorie page

--- src/app/dashboard/categories/categories-new/categories-new.component.ts
+++ src/app/dashboard/categories/categories-new/categories-new.component.ts
@@ -1,4 +1,7 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {Categorie} from '../../shared/models/categorie.model';
+import {TypeCategorie, TypeCategorieList} from '../../shared/enums/type-categorie';
 
 @Component({
   selector: 'app-categories-new',
@@ -6,14 +9,68 @@ import {Component, OnInit, ViewChild} from '@angular/core';
   styleUrls: ['./categories-new.component.scss']
 })
 export class CategoriesNewComponent implements OnInit {
-  constructor() {
+  public formCategorie: FormGroup = new FormGroup({});
+  public formCategorieList: FormGroup = new FormGroup({});
+  public categorie!: Categorie;
+  public typeCategorieList = TypeCategorieList;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    this.createFormCategorieList();
   }
 
   ngOnInit(): void {
+    this.createForm();
   }
 
   removeProduct(id: any): void {
     console.log('remove', id);
   }
 
+  private createForm(): void {
+    this.formCategorie = this.formBuilder.group({
+      id: new FormControl(this.categorie?.id ? this.categorie.id : null),
+      name: new FormControl(this.categorie?.name ? this.categorie.name : '', Validators.required),
+      type: this.formBuilder.array([])
+    });
+  }
+
+  private createFormCategorieList(): void {
+    this.formCategorieList = this.formBuilder.group({
+      typelist: this.formBuilder.array([])
+    });
+    this.initTypeList();
+  }
+
+  initTypeList(): void {
+    this.typeCategorieList.forEach(t => {
+      this.typeList.push(this.createTypeListForm(false, t));
+    });
+  }
+
+  get typeList(): FormArray {
+    return this.formCategorieList.get('typelist') as FormArray;
+  }
+
+  private createTypeListForm(checked: boolean, typeCategorie: TypeCategorie): FormGroup {
+    return new FormGroup({
+        checked: new FormControl(checked),
+        type: new FormControl(typeCategorie, Validators.required),
+      }
+    );
+  }
+
+  get type(): FormArray {
+    return this.formCategorie.get('type') as FormArray;
+  }
+
+  addTypeListCategorie(): void {
+    this.type.controls = [];
+    this.typeList.controls.forEach(t => {
+      if (t.value.checked) {
+        this.type.push(t.value.type);
+      }
+    });
+  }
 }
---
+++ src/app/dashboard/shared/services/categories.service.ts
@@ -0,0 +1,23 @@
+import {Injectable} from '@angular/core';
+import {environment} from '../../../../environments/environment';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Categorie} from '../models/categorie.model';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriesService {
+  api: string = environment.NZESPORTES_API + 'products/categories';
+
+  constructor(
+    private http: HttpClient
+  ) {
+  }
+
+  create(categorie: Categorie): Observable<Categorie> {
+    const params = new HttpParams()
+      .set('async', 'true');
+    return this.http.post<Categorie>(this.api, categorie, {params});
+  }
+}
