bcd3bbab5ae1195e4aeb12ed0a11a0a761629afb
nfandre <andrelive.05@hotmail.com>
2021-05-17 23:51:46 -0300

    feat: layout product

--- src/app/dashboard/products/new-product/new-product.component.ts
+++ src/app/dashboard/products/new-product/new-product.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
+import {SwalComponent} from '@sweetalert2/ngx-sweetalert2';
+import {Brand} from '../../shared/models/brand.model';
+import {Product} from '../../shared/models/product.model';
 
 @Component({
   selector: 'app-new-product',
@@ -6,10 +11,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-product.component.scss']
 })
 export class NewProductComponent implements OnInit {
+  @ViewChild('success')
+  public readonly dialogSuccess!: SwalComponent;
+  @ViewChild('error')
+  public readonly dialogError!: SwalComponent;
 
-  constructor() { }
+  public formProduct: FormGroup = new FormGroup({});
+  public product!: Product;
+  hasError!: boolean;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+  }
 
   ngOnInit(): void {
+    this.createForm();
+  }
+
+  private createForm(): void {
+    this.formProduct = this.formBuilder.group({
+      id: new FormControl(this.product?.id ? this.product.id : null),
+      description: new FormControl(this.product?.description ? this.product.description : '', Validators.required),
+      model: new FormControl(this.product?.model ? this.product.model : '', Validators.required),
+      category:  this.formBuilder.array(this.product?.category ? this.product.category : []),
+      productDetails:  this.formBuilder.array(this.product?.productDetails ? this.product.productDetails : []),
+      status: new FormControl(this.product?.status ? this.product.status : null),
+    });
+  }
+
+  save(): void {
+  }
+
+  redirect(): void{
   }
 
 }
--- src/app/dashboard/shared/services/products.service.ts
+++ src/app/dashboard/shared/services/products.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {environment} from '../../../../environments/environment';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Product} from '../models/product.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,4 +13,10 @@ export class ProductsService {
   constructor(
     private http: HttpClient
   ) { }
+
+  create(product: Product): Observable<Product> {
+    const params = new HttpParams()
+      .set('async', 'true');
+    return this.http.post<Product>(this.api, product, {params});
+  }
 }
