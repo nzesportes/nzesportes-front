7272bbfbe5b8021a3000f93474ce510db6a7678d
nfandre <andrelive.05@hotmail.com>
2021-05-24 22:40:08 -0300

    feat: error async loading and create pagination service

--- src/app/dashboard/products/products-list/products-list.component.ts
+++ src/app/dashboard/products/products-list/products-list.component.ts
@@ -7,9 +7,9 @@ import {take} from 'rxjs/operators';
 import {SwalComponent} from '@sweetalert2/ngx-sweetalert2';
 import {Router} from '@angular/router';
 import {ProductsService} from '../../../shared/services/products.service';
-import {CategoryPage} from '../../../shared/models/pagination-model/category-page.model';
 import {Product} from '../../../shared/models/product.model';
 import {ProductPage} from '../../../shared/models/pagination-model/product-page.model';
+import {PaginationService} from '../../../shared/services/pagination.service';
 
 @Component({
   selector: 'app-products-list',
@@ -22,10 +22,7 @@ export class ProductsListComponent implements OnInit {
   public formSearch!: FormGroup;
 
   products: Product[] = [];
-  page = 0;
   content: ProductPage | undefined;
-  pages: number | undefined;
-  public pageRange: any;
   hasError!: boolean;
 
   constructor(
@@ -34,13 +31,15 @@ export class ProductsListComponent implements OnInit {
     private categoriesService: CategoriesService,
     private productsService: ProductsService,
     private router: Router,
+    public paginationService: PaginationService
   ) {
   }
 
   ngOnInit(): void {
+    this.paginationService.initPagination();
     this.createForm();
     this.verifyHasBrandsCategories();
-    this.getAllProduct(10, this.page);
+    this.getAllProduct(10, this.paginationService.page);
   }
 
   getAllProduct(size: number, page: number): void {
@@ -49,37 +48,16 @@ export class ProductsListComponent implements OnInit {
       .subscribe(r => {
         this.products = r.content;
         this.content = r;
-        this.getPageRange();
+        this.paginationService.getPageRange(this.content.totalElements);
       }, () => {
         this.hasError = true;
       });
   }
 
-  getPageRange(): void {
-    // @ts-ignore
-    this.pages = Math.ceil(this.content?.totalElements / 10);
-
-    this.pageRange = {
-      first: this.page > 2 ? this.page - 3 : 0,
-      last: this.page > 2 && this.pages > this.page + 3 ? this.page + 3 :
-        this.page < 3 && this.pages >= 5 ? 5 :
-          this.page === this.pages || this.pages < 5 ?
-            this.pages : this.page + (this.pages - this.page)
-    };
-  }
-
-  getRange(): any[] {
-    const result = [];
-    for (let i = this.pageRange.first; i < this.pageRange.last; i++) {
-      result.push(i);
-    }
-    return result;
-  }
-
 
   updateIndex(index: number): void {
     this.getAllProduct(10, index);
-    this.page = index;
+    this.paginationService.page = index;
   }
 
   private createForm(): void {
