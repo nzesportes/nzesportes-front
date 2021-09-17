fb764c86e9ecd7fc81f4b5bc4a46963c14e1706c
rafaelpeinado <rafael.peinado.silva@gmail.com>
2021-09-17 01:55:29 -0300

    feat: atualiza filtros, ajusta fontawesome

--- src/app/store/template/product-detail/product-detail.component.ts
+++ src/app/store/template/product-detail/product-detail.component.ts
@@ -61,7 +61,7 @@ export class ProductDetailComponent implements OnInit {
     pullDrag: false,
     dots: false,
     navSpeed: 700,
-    navText: ['<i class=fas fa-chevron-left></i>', '<i class=fas fa-chevron-right></i>'],
+    navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
     responsive: {
       0: {
         items: 3
