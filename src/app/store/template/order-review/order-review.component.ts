c631d054f0df50442a688443ac61645273927670
rafaelpeinado <rafael.peinado.silva@gmail.com>
2021-09-30 14:17:14 -0300

    feat: integra pagamento

--- src/app/store/template/order-review/order-review.component.ts
+++ src/app/store/template/order-review/order-review.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {TokenStorageService} from '../../../shared/services/token-storage.service';
 import {AddressService} from '../../../shared/services/address.service';
 import {Address} from '../../../shared/models/address.model';
@@ -8,6 +8,9 @@ import {PurchaseService} from '../../../shared/services/purchase.service';
 import {Store} from '@ngrx/store';
 import * as fromActions from '../../redux/cart/cart.actions';
 import * as fromSelector from '../../redux/cart/cart.selectors';
+import {take} from 'rxjs/operators';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {PaymentTO} from '../../../shared/models/payment-to.model';
 
 @Component({
   selector: 'app-order-review',
@@ -16,26 +19,48 @@ import * as fromSelector from '../../redux/cart/cart.selectors';
 })
 export class OrderReviewComponent implements OnInit {
 
+  formGoToPayment!: FormGroup;
   isLogged = false;
   addresses: Address[] | undefined;
   products$!: Observable<ItemCart[]>;
   total$!: Observable<number>;
   isLoading$!: Observable<boolean>;
+  total!: number;
+  shipment!: number;
+  voucher = 5;
+  paymentTO: PaymentTO = {
+    products: [],
+    shipment: 0,
+    shipmentId: ''
+  };
 
   constructor(
     private tokenStorageService: TokenStorageService,
     private addressService: AddressService,
-    private purchase: PurchaseService,
+    private purchaseService: PurchaseService,
+    private formBuilder: FormBuilder,
     private store: Store<any>
-  ) { }
+  ) {
+  }
 
   ngOnInit(): void {
+    this.createForm();
     this.store.dispatch(fromActions.requestLoadProducts());
     this.products$ = this.store.select(fromSelector.products);
     this.total$ = this.store.select(fromSelector.total);
     this.isLoading$ = this.store.select(fromSelector.isLoading);
+    this.shipment = 20;
+
+    this.getPaymentTO();
+    this.getTotalPurchase();
     this.getAddressesByUser();
     this.isLogged = this.tokenStorageService.isLoggedIn();
+  }
+
+  createForm(): void {
+    this.formGoToPayment = this.formBuilder.group({
+      shipmentId: ['', Validators.required]
+    });
   }
 
   getAddressesByUser(): void {
@@ -45,6 +70,37 @@ export class OrderReviewComponent implements OnInit {
       }, error => {
         console.log(error);
       });
+  }
+
+  getPaymentTO(): void {
+    this.products$
+      .pipe(take(1))
+      .subscribe(response => {
+        response.forEach(product => {
+          this.paymentTO.products.push({
+            stockId: product?.stock.id,
+            quantity: product?.quantity
+          });
+        });
+      });
+    this.paymentTO.shipment = this.shipment;
+  }
+
+  getTotalPurchase(): void {
+    this.total$
+      .pipe(take(1))
+      .subscribe(response => {
+        this.total = response + this.shipment - this.voucher;
+      });
+  }
+
+  goToPayment(): void {
+    this.paymentTO.shipmentId = this.formGoToPayment?.get('shipmentId')?.value;
+    console.warn('TESTANDO', this.paymentTO);
+    this.purchaseService.createPaymentRequest(this.paymentTO)
+      .subscribe(response => {
+        window.open(response.paymentUrl, '_blank');
+      }, error => console.error('ERROR SERVICE', error));
   }
 
 }
