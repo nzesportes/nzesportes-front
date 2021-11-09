1224356f300556ceeff6efa016f74f1de5f9ce6a
rafaelpeinado <rafael.peinado.silva@gmail.com>
2021-11-08 21:59:44 -0300

    feat: atualiza tela de pedidos

---
+++ src/app/store/template/components/developed-by/developed-by.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-developed-by',
+  templateUrl: './developed-by.component.html',
+  styleUrls: ['./developed-by.component.scss']
+})
+export class DevelopedByComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
