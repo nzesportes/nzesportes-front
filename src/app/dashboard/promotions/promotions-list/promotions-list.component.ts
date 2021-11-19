24abda10adaaed9a82036bd14f2c2ef18f4888df
nfandre <andrelive.05@hotmail.com>
2021-11-18 23:54:36 -0300

    feat: crud promotions

--- src/app/dashboard/promotions/promotions-list/promotions-list.component.ts
+++ src/app/dashboard/promotions/promotions-list/promotions-list.component.ts
@@ -1,9 +1,12 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {SwalComponent} from '@sweetalert2/ngx-sweetalert2';
 import {Sale} from '../../../shared/models/sale.model';
-import {FormGroup} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 import {PaginationService} from '../../../shared/services/pagination.service';
 import {SalePage} from '../../../shared/models/pagination-model/sale-page.model';
+import {take} from 'rxjs/operators';
+import {ProductsService} from '../../../shared/services/products.service';
+import {SalesService} from '../../../shared/services/sales.service';
 
 @Component({
   selector: 'app-promotions-list',
@@ -20,33 +23,51 @@ export class PromotionsListComponent implements OnInit {
   promotions: Sale[] = [];
   hasError!: boolean;
   formPromotions!: FormGroup;
+  formCoupons!: FormGroup;
 
 
   constructor(
-    public paginationService: PaginationService
+    public paginationService: PaginationService,
+    public salesService: SalesService,
+    private formBuilder: FormBuilder,
   ) { }
 
   ngOnInit(): void {
-    const sale: Sale = {
-      percentage: 10,
-      endDate: new Date(),
-      startDate: new Date(),
-      quantityLeft: 89,
-      id: '123',
-      quantity: 100
-    };
-    this.promotions.push(sale);
+    this.hasError = false;
+    this.paginationService.initPagination();
+    this.createForm();
+    this.getAllPromotions(10, this.paginationService.page);
+  }
+
+
+  getAllPromotions(size: number, page: number, code?: string): void {
+    this.salesService.getAll(size, page, code)
+      .pipe(take(1))
+      .subscribe(r => {
+        this.promotions = r.content;
+        this.content = r;
+        this.paginationService.getPageRange(this.content.totalElements);
+      }, () => {
+        this.hasError = true;
+      });
+  }
+  onChangeFilter(): void {
+    this.paginationService.initPagination();
+    // const code = this.formCoupons.get('code')?.value;
+    this.getAllPromotions(10, 0, '');
   }
 
 
   updateIndex(index: number): void {
-    // this.getAll(10, index);
+    this.getAllPromotions(10, index);
     this.paginationService.page = index;
   }
 
-
-  redirect(): void {
-
+  private createForm(): void {
+    this.formPromotions = this.formBuilder.group({
+      status: new FormControl(),
+    });
   }
+
 
 }
