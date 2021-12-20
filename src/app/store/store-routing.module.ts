5e41db6d6f5e0d4dc7b329516b668f09613d953a
nfandre <andrelive.05@hotmail.com>
2021-12-20 08:37:09 -0300

    fix: router user account

--- src/app/store/store-routing.module.ts
+++ src/app/store/store-routing.module.ts
@@ -70,7 +70,8 @@ const routes: Routes = [
       {
         path: 'minha-conta',
         // canActivate: [AuthGuard],
-        loadChildren: () => import('./template/account-details/account-details.module').then(m => m.AccountDetailsModule)
+        loadChildren: () => import('./template/account-details/account-details.module').then(m => m.AccountDetailsModule),
+        canActivate: [AuthGuard]
       },
       {
         path: 'avaliar',
